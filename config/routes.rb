784a493108bec79c859fe6cc5bebf0911c19ede0
Vince Banzon <vince@launchlabs.ph>
2015-05-04 12:09:50 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'hom  e/index'
 
-  resources :questions
+  resources :questions do 
+    resources :answers
+  end
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
