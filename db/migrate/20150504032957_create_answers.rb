6da7a9cf0a82deb0679b5c9ccb129a5089eaf69f
Vince Banzon <vince@launchlabs.ph>
2015-05-04 12:01:32 +0800

    migration id

--- db/migrate/20150504032957_create_answers.rb
+++ db/migrate/20150504032957_create_answers.rb
@@ -2,6 +2,8 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.text :content
+      t.integer :question_id
+      t.integer :user_id
       
 
       t.timestamps null: false
