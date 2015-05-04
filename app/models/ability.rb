16b50221318389928aca647cb3244cfe95ed9102
Vince Banzon <vince@launchlabs.ph>
2015-05-04 16:46:03 +0800

    Finish

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,34 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+
+
+
+    if user.admin?
+
+        can :manage, :all
+
+
+    else
+
+        can :update, Question do |question|
+            question.user == user
+        end
+
+        can :destroy, Question do |question|
+            question.user == user
+        end 
+
+        can :destroy, Answer do |question|
+            question.user == user
+        end
+
+        can :create, Question
+        can :create, Answer
+
+
+
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
